9ea959de67d4425385d00de9dc50bdf09beeed2e
Andreas Färber <afaerber@suse.de>
2014-11-27 08:13:50 +0100

    ARM: STM32: Prepare RCC
    Signed-off-by: Andreas Färber <afaerber@suse.de>

---
+++ include/dt-bindings/clock/stm32f42xxx.h
@@ -0,0 +1,24 @@
+#define CLK_HSI		0
+#define CLK_HSE		1
+
+#define CLK_PLLSRC	2
+#define CLK_PLL		3
+#define CLK_PLLCLK	4
+#define CLK_PLL48CK	5
+
+#define CLK_SYSCLK	6
+
+#define CLK_AHB		7
+
+#define CLK_APB1	8
+#define CLK_TIM2	9
+#define CLK_TIM3	10
+#define CLK_TIM4	11
+#define CLK_TIM5	12
+#define CLK_USART3	13
+
+#define CLK_APB2	14
+#define CLK_USART1	15
+#define CLK_USART6	16
+
+#define CLK_NR_CLKS	17
